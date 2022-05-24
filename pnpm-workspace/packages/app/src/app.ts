3aa7168e3d06dbe3cb401c176b631ffc8b42b4f7
Nils Hartmann <nils@nilshartmann.net>
2022-05-24 21:36:23 +0200

    3.0

--- pnpm-workspace/packages/app/src/app.ts
+++ pnpm-workspace/packages/app/src/app.ts
@@ -2,6 +2,7 @@ import { sayHello } from "@nilshartmann/lib";
 import { sayHelloToUtils } from "@nilshartmann/utils";
 
 export function app() {
+  console.log("app - 3.0");
   console.log("app - sayHello", sayHello());
   console.log("app - sayHelloToUtils", sayHelloToUtils());
 }
